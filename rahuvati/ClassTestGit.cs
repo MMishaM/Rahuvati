2ab42b26b06d255c358a4aa4922277545cf5d9a1
Mikhail <mishamishamisha1996@gmail.com>
2017-08-02 20:15:15 +0300

    add new class

---
+++ rahuvati/ClassTestGit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rahuvati
+{
+    class ClassTestGit
+    {
+
+        public static void multiplyArray(int[] a , int b)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+
+            }
+        }
+    }
+}
